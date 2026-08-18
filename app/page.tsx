8c4652db60f1bd59a8a4319d27d5f6ac7853b5df
altchadkag-sys <altchadkag@gmail.com>
2026-08-18 18:06:39 -0400

    Fix HTML tag formatting in page.tsx Update Accueil with original R & RA content

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
             </a>
             <a href="#mission" className="hover:text-blue-700">
               Notre mission
-            </a>
+            </a> 
             <a href="/recherche" className="hover:text-blue-700">
               Recherche
             </a>
