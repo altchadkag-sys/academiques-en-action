29074ac0fc60cd9bb6dd22571fca758e0f35e24e
altchadkag-sys <altchadkag@gmail.com>
2026-08-22 22:51:31 -0400

    Update Recherche link to use anchor instead of path

--- app/page.tsx
+++ app/page.tsx
@@ -20,9 +20,9 @@ export default function Home() {
             <a href="#mission" className="hover:text-blue-700">
               Notre mission
             </a> 
-            <a href="/recherche" className="hover:text-blue-700">
-              Recherche
-            </a>
+            <a href="#recherche" className="hover:text-blue-700">
+  Recherche
+</a>
             <a href="#formation" className="hover:text-blue-700">
               Formation
             </a>
